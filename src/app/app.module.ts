4fb208025df20a4ee362a3b1c67dce88bf799652
salaheddine.charko <salaheddine.charko@gmail.com>
2022-02-02 21:23:22 +0100

    tp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+
+import { FormsModule ,ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Exercice1Component } from './exercice1/exercice1.component';
+import { Exercice2Component } from './exercice2/exercice2.component';
+import { Exercice3Component } from './exercice3/exercice3.component';
+import { AcceuilComponent } from './exercice3/acceuil/acceuil.component';
+import { DetailsComponent } from './exercice3/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Exercice1Component,
+    Exercice2Component,
+    Exercice3Component,
+    AcceuilComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
